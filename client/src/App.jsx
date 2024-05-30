b648d1f8026eeb4b37c0ed80b79f5111a2501dc5
AkshatPat3l <akshatpat3l@gmail.com>
2024-05-29 20:09:19 -0400

    Configured Tailwind CSS and Flowbite React, added page, page routes and finished header component

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
