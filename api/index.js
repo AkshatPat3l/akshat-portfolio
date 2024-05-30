f1ba54b721f5f70423b89dd15dca9170bdbd2aeb
AkshatPat3l <akshatpat3l@gmail.com>
2024-05-29 20:59:10 -0400

    Create Test API

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -12,7 +13,7 @@ mongoose
   });
 
 const app = express();
-
+app.use("/api/user", userRoutes);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
