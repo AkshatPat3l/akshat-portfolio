5d3d4662dc33ab02e68e1f4a24e12e0bcf04e145
AkshatPat3l <akshatpat3l@gmail.com>
2024-06-05 23:47:23 -0400

    Create Update User API

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { testAPI } from "../controllers/user.controller.js";
+import { testAPI, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", testAPI);
-
+router.put("/update/:userId", verifyToken, updateUser);
 
 export default router;
